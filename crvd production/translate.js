8cbfe064bfed525157e683e2c5127b1e1327f8af
Ehab Hossam <ehabhossam87@gmail.com>
2022-08-18 01:25:31 +0200

    Add files via upload

---
+++ crvd production/translate.js
@@ -0,0 +1,148 @@
+let arrLang = {
+    en: {
+        'navHome': 'Home',
+        'navProjects': 'Projects',
+        'navServices': 'Services',
+        'navTeam': 'Team',
+        'navContact': 'Contact Us',
+
+        'home': "Technologies have become full of curves Let's overcome those curves together.",
+        'about': 'About Us',
+        'homeContactBtn': 'Contact Us',
+        'homeServiceBtn': 'Service',
+
+        'serviesWord': 'Our Services,',
+        'servicesparagraph1': 'When you need a better way to deliver your services,',
+        'servicesparagraph2': 'Practical advice & Unique services.',
+        'servicesparagraph3': 'There is no better place to be than at',
+        'servicesparagraph4': 'C R V D',
+        'servicesMainParagraphHeader': 'What We Can Do',
+        'servicesMainParagraph': "We believe in using human-centered design to improve peoples lives through digital products and services. At CRVD we help companies understand their customers and use that knowledge to improve businesses, together we can go to great lengths to make yourbusiness looks good. What we offer ? we 're offering you UI/UX Design, Web Development, Mobile Application Development, Desktop Application, Graphics and SEO.  Our Top Priority is your Success.",
+
+        'teamHeader': 'Our Experts',
+        'teamPragraph': 'Let\'s meet with our team members',
+        'employeeName1': 'Ehab Hossam',
+        'employeeName2': 'Ehab Hossam',
+        'employeeName3': 'Ehab Hossam',
+        'employeeName4': 'Ehab Hossam',
+        'employeeJob1': 'Web Developer',
+        'employeeJob2': 'Flutter Developer',
+        'employeeJob3': 'Web Developer',
+        'employeeJob4': 'Flutter Developer',
+
+        'projectsHeader': 'As a UX design company whose passion is web interface, we help companies bring ideas to life. We are a company that listens to our customers and is passionate about creating products and services that transform the everyday experience of working with technology.',
+        'modernWord': 'MODERN',
+        'number1': "NO. 1",
+        'modernParagraph': 'Crvd helps companies to solve complex user experience problems. We are your User Experience design and consulting partner.',
+        'flexibleWord': 'flexible',
+        'number2': "NO. 2",
+        'flexibleParagraph': 'UX is more than wireframes and prototypes. It’s a process of understanding people, creating empathy, and building valuable experiences for your users.',
+        'modernWord': 'Style',
+        'number3': "NO. 3",
+        'styleParagraph': 'CRVD is a personal project that explores and discover innovative ways to present food in restaurants through photography and styling.',
+        'useableWord': 'useable',
+        'number4': "NO. 4",
+        'useableParagraph': 'System Crashers is a cyber security services website which provide their clients the required safety for their businesses.',
+        'innovateWord': 'innovate',
+        'number5': "NO.5",
+        'modernParagraph': 'We make your images stand out. We care about the details that make you unique and different as a photographer.',
+        'simpleWord': 'Simple',
+        'number6': "NO.6",
+        'simpleParagraph': 'We want to build the world\’s best user interface. The future of UI is here! CRVD strives to give their clients the best front end possible.',
+
+        'contact': 'Contact US',
+        'contactWord': 'CONTACT',
+        'sendWord': 'SEND',
+        'location': 'Egypt and Saudi Arabia',
+        'copyrights': '&copy; ALL RIGHTS RESERVED FOR CRVD'
+    },
+
+    ar: {
+        'navHome': 'الرئيسيه',
+        'navProjects': 'اعمالنا',
+        'navServices': 'خدماتنا',
+        'navTeam': 'الفريق',
+        'navContact': 'تواصل معنا',
+
+        'home': 'أصبحت التقنيات مليئة بالمنحنيات دعونا نتغلب على تلك المنحنيات معًا.',
+        'homeContactBtn': 'تواصل معنا',
+        'homeServiceBtn': 'خدماتنا',
+
+        'serviesWord': 'خدماتنا,',
+        'servicesparagraph1': 'عندما تحتاج إلى طريقة أفضل لتقديم خدماتك ،',
+        'servicesparagraph2': 'نصائح عملية وخدمات فريدة.',
+        'servicesparagraph3': 'لا يوجد مكان أفضل للتواجد فيه اكثر من',
+        'servicesparagraph4': 'C R V D',
+        'servicesMainParagraphHeader': 'ماذا نقدم؟',
+        'servicesMainParagraph': "نحن نؤمن باستخدام التصميم الذي يركز على الإنسان لتحسين حياة الناس من خلال المنتجات والخدمات الرقمية. في CRVD ، نساعد الشركات على فهم عملائها واستخدام تلك المعرفة لتحسين الأعمال ، يمكننا معًا بذل جهود كبيرة لجعل عملك يبدو جيدًا.  ماذا نقدم؟ نحن نقدم لك تصميم UI / UX ، وتطوير الويب ، وتطوير تطبيقات الهاتف المحمول ، وتطبيقات سطح المكتب ، والرسومات ، وتحسين محركات البحث. أولويتنا القصوى هي نجاحك.",
+
+        'teamHeader': 'خبرائنا',
+        'teamPragraph': 'دعونا نجتمع مع أعضاء فريقنا',
+        'employeeName1': 'ايهاب حسام',
+        'employeeName2': 'ايهاب حسام',
+        'employeeName3': 'ايهاب حسام',
+        'employeeName4': 'ايهاب حسام',
+        'employeejob1': 'مطور ويب',
+        'employeejob2': 'مطور موبايل ابلكيشن',
+        'employeejob3': 'مطور ويب',
+        'employeejob4': 'مطور موبايل ابلكيشن',
+
+        'projectsHeader': 'بصفتنا شركة تصميم تجربة المستخدم التي يتمثل شغفها بواجهة الويب ، فإننا نساعد الشركات على إحياء الأفكار. نحن شركة تستمع لعملائنا ومتحمسون لإنشاء المنتجات والخدمات التي تحول التجربة اليومية للعمل مع التكنولوجيا.',
+        'modernWord': 'عصري',
+        'number1': "رقم. 1",
+        'modernParagraph': 'يساعد Crvd الشركات على حل مشاكل تجربة المستخدم المعقدة. نحن شريكك الاستشاري في تصميم تجربة المستخدم.',
+        'flexibleWord': 'مرن',
+        'number2': "رقم. 2",
+        'flexibleParagraph': 'تجربة المستخدم هي أكثر من مجرد إطارات سلكية ونماذج أولية. إنها عملية فهم الأشخاص وخلق التعاطف وبناء تجارب قيمة للمستخدمين.',
+        'modernWord': 'أناقة',
+        'number3': "رقم. 3",
+        'styleParagraph': 'CRVD هو مشروع شخصي يستكشف ويكتشف طرقًا مبتكرة لتقديم الطعام في المطاعم من خلال التصوير الفوتوغرافي والتصميم.',
+        'useableWord': 'صالح للاستخدام',
+        'number4': "رقم. 4",
+        'useableParagraph': 'يسؤخسيؤعيسعخععخععخخعسيعخيسعخسيعخسيعيسعخعيسعيسخعخسيعيسخعيسعخخعسيعخيسعخيسعيسخععخ',
+        'innovateWord': 'مبتكر',
+        'number5': "رقم. 5",
+        'modernParagraph': 'نحن نجعل صورك بارزة نحن نهتم بالتفاصيل التي تجعلك فريدًا ومختلفًا كمصور.',
+        'simpleWord': 'بسيط',
+        'number6': "رقم. 6",
+        'simpleParagraph': 'نريد بناء أفضل واجهة مستخدم في العالم. مستقبل واجهة المستخدم هنا! تسعى CRVD جاهدة لمنح عملائها أفضل واجهة أمامية ممكنة.',
+
+        'contactWord': 'تواصل معنا',
+        'sendWord': 'ارسال',
+        'location': 'مصر والمملكه العربيه السعوديه',
+        'copyrights': 'CRVD حقوق الملكيه محفوظه الى '
+
+
+    }
+}
+
+
+$(function() {
+    let lang = localStorage.getItem('language');
+    changeLanguage(lang);
+
+
+    $('.translate').click(function() {
+        lang = $(this).attr('id');
+        localStorage.setItem('language', lang);
+        changeLanguage(lang);
+    });
+
+    function changeLanguage(lang) {
+        $('.lang').each(function(index, element) {
+            $(this).text(arrLang[lang][$(this).attr('key')]);
+        });
+    }
+
+})
+
+const eng = document.getElementById('eng');
+const arb = document.getElementById('ar');
+const pages = document.getElementsByTagName('*');
+arb.addEventListener('click', () => {
+    for (let index = 0; index < pages.length; index++) {
+        pages[index].style.letterSpacing = 'unset';
+
+    }
+
+});
